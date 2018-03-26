c0b172cf714c65c141a46745c337ca445e035d99
milkshakeiii <milkshakeiii@gmail.com>
2018-03-26 14:18:13 -0400

    isolate obj data gathering, webgl template

--- Assets/ObjSpawner.cs
+++ Assets/ObjSpawner.cs
@@ -19,47 +19,43 @@ public class ObjSpawner : MonoBehaviour
 		if (!this.enabled)
 			return;
 
-		string[] filesInFolder = System.IO.Directory.GetFiles(pathToFolderWithObjs);
+		Dictionary<string, byte[]> nameToObjDict = GetNameToObjDict();
 		List<GameObject> loadedObjs = new List<GameObject> ();
 
-		foreach (string filePath in filesInFolder)
+		foreach (string objName in nameToObjDict.Keys)
 		{
-			if (System.IO.Path.GetExtension (filePath).Equals (".obj"))
+			if (objName.Contains("pial"))
+				continue;
+
+			// Load the .obj using TriLib
+			GameObject targetObject;
+			using (TriLib.AssetLoader assetLoader = new TriLib.AssetLoader())
 			{
-				string fileName = System.IO.Path.GetFileName (filePath);
-				if (fileName.Contains("pial"))
-					continue;
-
-				// Load the .obj using TriLib
-				GameObject targetObject;
-				using (TriLib.AssetLoader assetLoader = new TriLib.AssetLoader())
-				{
-					byte[] fileData = System.IO.File.ReadAllBytes(filePath);
-					targetObject = assetLoader.LoadFromMemory(fileData, ".obj");
-				}
-
-				targetObject.name = fileName;
-				loadedObjs.Add (targetObject);
-
-				//set up the components
-				GameObject importantChild = targetObject.GetComponentInChildren<MeshRenderer>().gameObject;
-				//GameObject importantChild = targetObject.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
-				importantChild.AddComponent<MeshCollider>();
-				importantChild.AddComponent<MouseOverOutline> ().outline = importantChild.AddComponent<cakeslice.Outline> ();
-				importantChild.AddComponent<BrainPiece> ();
-				importantChild.name = targetObject.name;
-
-				if (fileName.Contains ("hcp") && fileName.Contains ("lh"))
-					targetObject.transform.parent = hcpLeftParent.transform;
-				else if (fileName.Contains ("hcp") && fileName.Contains ("rh"))
-					targetObject.transform.parent = hcpRightParent.transform;
-				else if (fileName.Contains ("lh"))
-					targetObject.transform.parent = dkLeftParent.transform;
-				else if (fileName.Contains ("rh"))
-					targetObject.transform.parent = dkRightParent.transform;
-				else
-					throw new UnityException ("An obj with an unrecorgnized naming exists.");
+				byte[] fileData = nameToObjDict[objName];
+				targetObject = assetLoader.LoadFromMemory(fileData, ".obj");
 			}
+
+			targetObject.name = objName;
+			loadedObjs.Add (targetObject);
+
+			//set up the components
+			GameObject importantChild = targetObject.GetComponentInChildren<MeshRenderer>().gameObject;
+			//GameObject importantChild = targetObject.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
+			importantChild.AddComponent<MeshCollider>();
+			importantChild.AddComponent<MouseOverOutline> ().outline = importantChild.AddComponent<cakeslice.Outline> ();
+			importantChild.AddComponent<BrainPiece> ();
+			importantChild.name = targetObject.name;
+
+			if (objName.Contains ("hcp") && objName.Contains ("lh"))
+				targetObject.transform.parent = hcpLeftParent.transform;
+			else if (objName.Contains ("hcp") && objName.Contains ("rh"))
+				targetObject.transform.parent = hcpRightParent.transform;
+			else if (objName.Contains ("lh"))
+				targetObject.transform.parent = dkLeftParent.transform;
+			else if (objName.Contains ("rh"))
+				targetObject.transform.parent = dkRightParent.transform;
+			else
+				throw new UnityException ("An obj with an unrecorgnized naming exists.");
 		}
 		Debug.Log ("Load finished");
 
@@ -67,5 +63,19 @@ public class ObjSpawner : MonoBehaviour
 			monoBehavior.enabled = true;
 		foreach (GameObject disableMe in disableWhenFinished)
 			disableMe.SetActive (false);
+	}
+
+	private Dictionary<string, byte[]> GetNameToObjDict()
+	{
+		Dictionary<string, byte[]> nameToObjDict = new Dictionary<string, byte[]> ();
+		string[] filePaths = System.IO.Directory.GetFiles (pathToFolderWithObjs);
+		foreach (string filePath in filePaths)
+		{
+			if (System.IO.Path.GetExtension (filePath).Equals (".obj"))
+			{
+				nameToObjDict.Add (System.IO.Path.GetFileName (filePath), System.IO.File.ReadAllBytes (filePath));
+			}
+		}
+		return nameToObjDict;
 	}
 }
