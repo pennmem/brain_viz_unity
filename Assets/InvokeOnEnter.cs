bab506169695be0cd747370fd9d7baf66a6aa7cb
milkshakeiii <milkshakeiii@gmail.com>
2018-04-13 17:30:54 -0400

    fix issues

---
+++ Assets/InvokeOnEnter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeOnEnter : MonoBehaviour
+{
+
+	public UnityEngine.UI.InputField inputField;
+	public UnityEngine.UI.Button invokeMe;
+
+	private void Start()
+	{
+		inputField.onEndEdit.AddListener(val =>
+			{
+				if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+					invokeMe.onClick.Invoke();
+			});
+	}
+
+}
