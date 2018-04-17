7617ebddfad28ba424657822b314f9bb66182e77
milkshakeiii <milkshakeiii@gmail.com>
2018-04-17 11:55:05 -0400

    no wiggle

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -20,18 +20,21 @@ public class Spinner : MonoBehaviour
 		gameObject.transform.RotateAround (spinCenter, new Vector3 (1, 0, 0), xSpin);
 		gameObject.transform.RotateAround (spinCenter, new Vector3 (0, 0, 1), zSpin);
 
+		Vector2 mousePos = Input.mousePosition;
 		if (Input.GetMouseButtonDown (0))
-			lastMousePos = Input.mousePosition;
+			lastMousePos = mousePos;
 		if (Input.GetMouseButton (0))
 		{
-			Vector2 newMousePos = Input.mousePosition;
-			float Xchange = newMousePos.x - lastMousePos.x;
-			float Ychange = newMousePos.y - lastMousePos.y;
+			if (mousePos.x < Screen.width * Camera.main.rect.width)
+			{
+				float Xchange = mousePos.x - lastMousePos.x;
+				float Ychange = mousePos.y - lastMousePos.y;
 
-			gameObject.transform.RotateAround (spinCenter, new Vector3 (0, 1, 0), Xchange * mouseSpinFactor * Time.deltaTime);
-			gameObject.transform.RotateAround (spinCenter, new Vector3 (1, 0, 0), Ychange * mouseSpinFactor * Time.deltaTime);
+				gameObject.transform.RotateAround (spinCenter, new Vector3 (0, 1, 0), Xchange * mouseSpinFactor * Time.deltaTime);
+				gameObject.transform.RotateAround (spinCenter, new Vector3 (1, 0, 0), Ychange * mouseSpinFactor * Time.deltaTime);
 
-			lastMousePos = newMousePos;
+				lastMousePos = mousePos;
+			}
 		}
 
 
